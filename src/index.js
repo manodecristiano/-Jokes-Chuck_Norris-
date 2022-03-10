627687e4afdd6eeccf74d7043ff1e5a7b2034138
manodecristiano <cristian.carre.online@gmail.com>
2022-03-10 11:17:56 +0100

    http connection

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
+import { obtenerChiste } from "./js/http-provider";
 
-const jokeUrl = 'https://api.chucknorris.io/jokes/random';
 
-fetch( jokeUrl ).then ( response => {
-
-    response.json().then(data => {
-        console.log(data.id);
-        console.log(data.value);
-    })
-    
-    console.log(response);
-})
-  
-    
---
+++ src/js/http-provider.js
@@ -0,0 +1,61 @@
+//LOGICA PARA PETICIONES
+
+
+const jokeUrl = 'https://api.chucknorris.io/jokes/random5';
+
+
+const obtenerChiste = async () => { 
+
+ try {
+    const resp = await fetch( jokeUrl );
+
+    if( !resp.ok )return alert('No se puede realizar la peticion');
+ 
+    const  {icon_url, id , value} = await resp.json();
+    return {icon_url, id , value};
+
+ } catch (error) {
+     
+    throw error;
+
+ }
+
+}
+
+
+
+
+
+
+
+
+
+export {
+
+    obtenerChiste
+}
+
+
+/* APUNTES
+
+const jokeUrl = 'https://api.chucknorris.io/jokes/random';
+
+fetch( jokeUrl ).then ( response => {
+
+    response.json().then(data => {
+        console.log(data.id);
+        console.log(data.value);
+    })
+    
+    //esto de arriba es lo mismo que-->
+     fetch( jokeUrl )
+       .then( resp => resp.json() )
+       .then( ({ id, value }) => {
+         console.log(id+"<br>"+ value);
+       })
+
+
+    
+})
+
+*/
